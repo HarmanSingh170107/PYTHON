09af6b24fbe77e874d46331b3705a698057b7477
HarmanSingh170107 <harmansinghlamba007@gmail.com>
2024-07-08 19:34:54 +0530

    Add files via upload

---
+++ CHAPTER-10/WHAT IS CLASS.py
@@ -0,0 +1,18 @@
+# class is blueprint for creating object
+# class is kinda a thing that is empty and 
+# When filled becomes your object.
+# Consider class as a blank form of college and
+# When filed it becomes your filled form like,i.e.- an object 
+# So like that remember
+#Ex. of class:-
+class Employee:
+    name='hsl'
+    language='PY'
+    salary=10000 # **name,salary,language are class attributes**
+
+hsl1= Employee()#Any name like hsl1 or any other variable can be used here and below AND MOST IMPORTNATLY HERE *hsl1* is an *object* and also **memory mein class ka data tabhi jaata hai jab object banta hai so remember** imp-*,**-v.imp
+print(hsl1.name,hsl1.language,hsl1.salary)
+# NOW SOME EXAMPLES:-
+soham=Employee()
+soham.name="Soham Sikdar"
+print(soham.name,soham.salary,soham.language)
